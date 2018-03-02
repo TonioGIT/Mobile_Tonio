bf36a13db7b1427f77c00852185b9345a00fcd11
Tonio <antoine.doussan@campus-numerique-in-the-alps.com>
2018-03-02 14:18:53 +0100

    Affiche liste machines via boucle sur data.

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,17 @@
   <div id="app">
     <img src='./assets/singe.jpg'>
     <h1>{{ msg }}</h1>
-    <h2>Au boulooot... :(</h2>
+    <br>
+    <!-- <h2>Monkey machine #1: {{ machine1}}</h2>
+    <h2>Monkey machine #2: {{ machine2}}</h2>
+    <h2>Monkey machine #3: {{ machine3}}</h2> -->
+
+    <ul>
+      <li v-for="machine in machines">
+        {{ machine.id }} : {{ machine.name }}
+      </li>
+    </ul>
+
     </div>
 </template>
 
@@ -11,7 +21,15 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Mes machines à monkeys...'
+      msg: 'Mes machines à monkeys...',
+      // machine1: 'Yvan l\'orang outan.',
+      // machine2: 'Mac le macaque.',
+      // machine3: 'Zézé le chimpanzé.',
+      machines: [
+        {id:1, name:'Yvan l\'orang outan.'},
+        {id:2, name:'Mac le macaque.'},
+        {id:3, name:'Zézé le chimpanzé.'}
+      ]
     }
   }
 }
@@ -37,8 +55,9 @@ ul {
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+  // display: inline-block;
+  margin: 50px;
+  font-size: 30px;
 }
 
 a {
